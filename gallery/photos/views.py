715ae625fc6e53ee87595dfe96831916e1893004
munenelewis <munene2379@gmail.com>
2018-02-23 11:28:16 +0300

    changed the position of my  photo module because the urls were not picking it

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-'''started defining my functions'''
 def welcome(request):
-    return HTTPResponse('welcome to my gallery')
+
+    return HttpResponse('welcome to my page')
