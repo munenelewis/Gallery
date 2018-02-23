6619746cc1d93498ecfd69cb34d36b55b44857f4
munenelewis <munene2379@gmail.com>
2018-02-23 15:46:43 +0300

    created a test.py to test all my function.. instance function passed

---
+++ gallery/photos/test.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Image,Location,Category
+
+class ImageTest(TestCase):
+
+    def setUp(self):
+
+        self.img3 = Image(name= 'image3', description = 'the guys')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img3, Image))
