a281746a496ac8fd1fccf436406e0ee37089739f
munenelewis <munene2379@gmail.com>
2018-02-23 14:57:47 +0300

    added a one to one relationship to my models ie location and categoey to Image class

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    name = models.CharField(max_length = 40)
+    name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 100)
+    location = models.ForeignKey(Location)
+    category = moels.ForeignKey(Category)
+    def __str__(self):
+        return self.name
+class Location(models.Model):
+    location = models.CharField(max_length = 30)
+
+class Category(models.Model):
+    category = models.CharField(max_length = 30)
 
