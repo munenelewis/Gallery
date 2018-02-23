101bdbde0cd72639780d60f8926cfe041fe71b82
munenelewis <munene2379@gmail.com>
2018-02-23 15:46:10 +0300

    location and category were not being found so i moved the up the inage class.. a added null = true to make the feilds readerbale

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length = 30)
+    def __str__(self):
+            return self.location
+class Category(models.Model):
+    category = models.CharField(max_length = 30)
+    def __str__(self):
+            return self.category
 class Image(models.Model):
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 100)
-    location = models.ForeignKey(Location)
-    category = moels.ForeignKey(Category)
+    location = models.ForeignKey(Location, blank=True, null=True)
+    category = models.ForeignKey(Category, blank=True, null=True)
+    
     def __str__(self):
         return self.name
-class Location(models.Model):
-    location = models.CharField(max_length = 30)
-
-class Category(models.Model):
-    category = models.CharField(max_length = 30)
 
