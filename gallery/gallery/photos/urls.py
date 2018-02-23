55d2e298f62018d8497803793049d425acb7a0d1
munenelewis <munene2379@gmail.com>
2018-02-23 10:59:09 +0300

    created a photo/url page and also renamed it to urls

--- gallery/gallery/photos/urls.py
+++ gallery/gallery/photos/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-
+from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^photos/',include('photos.urls') ),#url function for the view function called welcome
+    url(r'^$', views.welcome, name = 'welcome'),#url function for the view function called welcome
 ]
