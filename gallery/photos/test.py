062b378c8f13d2cccd612c4bba436187542f7bd4
munenelewis <munene2379@gmail.com>
2018-02-23 16:41:04 +0300

    created test  for save and delete and they all passed

--- gallery/photos/test.py
+++ gallery/photos/test.py
@@ -6,6 +6,24 @@ class ImageTest(TestCase):
     def setUp(self):
 
         self.img3 = Image(name= 'image3', description = 'the guys')
+        
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.img3, Image))
+    #testing for saving images
+
+    def test_save_imge(self):
+        self.img3.save_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image)> 0)
+
+    def test_delete(self):
+        self.img3.save_image()
+        image = Image.objects.all()
+        self.img3.delete_image()
+        self.assertTrue(len(image) < 1)
+
