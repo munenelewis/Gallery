c1cd17910661b7511f60e1d37e98e49401c714f2
munenelewis <munene2379@gmail.com>
2018-02-23 16:41:33 +0300

    made a test for instantiating

--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
-# Create your tests here.
+class ImageTest(TestCase):
+
+    def setUp(self):
+
+        self.img3 = Image(name= 'image3', description = 'the guys')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img3, Image))
+    #
