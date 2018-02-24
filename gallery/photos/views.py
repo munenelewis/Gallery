1ad849897ad2b02cd896f2c77699e874b2e7c893
munenelewis <munene2379@gmail.com>
2018-02-25 01:17:54 +0300

    add a search function.. created and if statment to check if the search request is made

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from .models import Image,Location,Category
 from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
+  
+    photos = Image.objects.all()
+    arg =  { "photos": photos}
 
-    return render(request, 'welcome.html')
+    return render(request, 'welcome.html',arg)
+
+def search_results(request):
+
+    if 'photos' in request.GET and request.GET['photos']:
+        search_term = request.GET.get('photos')
+        search_photos = Image.search_category(search_term)
+
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{'message':message,'photos':search_photos})
+
+    else:
+        message = 'you havent searched anything'
+
+        return render(request,'search.html',{'message':message})
