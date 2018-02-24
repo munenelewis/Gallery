04e55010e7bb4b4109f628f2c9fd0448c978f620
munenelewis <munene2379@gmail.com>
2018-02-25 01:18:57 +0300

    added a classmethod with a function photo_url to help in getting the photourl

--- gallery/photos/models.py
+++ gallery/photos/models.py
@@ -14,6 +14,8 @@ class Image(models.Model):
     description = models.CharField(max_length = 100)
     location = models.ForeignKey(Location, blank=True, null=True)
     category = models.ForeignKey(Category, blank=True, null=True)
+    image_url = models.ImageField(upload_to = 'images/',  null=True)
+
 
     def save_image(self):
         self.save()
@@ -23,4 +25,15 @@ class Image(models.Model):
     
     def __str__(self):
         return self.name
-
+    @classmethod
+    def get_image(cls, location):
+        photos = cls.objects.filter(location = location)
+        return photos
+    @classmethod
+    def search_category(cls, search_term) :
+        photos = cls.objects.filter(category__icontains=search_term)
+        return photos
+    @property
+    def photo_url(self):
+        if self.image_url and hasattr(self.image_url, 'url'):
+            return self.image_url.url
