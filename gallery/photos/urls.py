d336372a6b8eddfd60749110220beebd568dcf84
munenelewis <munene2379@gmail.com>
2018-02-25 01:20:25 +0300

    added a if setting debug statment to help in image display

--- gallery/photos/urls.py
+++ gallery/photos/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome')
+    url('^$', views.welcome, name = 'welcome'),
+    url(r'^search', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
